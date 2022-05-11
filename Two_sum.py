6b835331ecf7f4af13c27cfa6615475f11e20e87
alina1124 <dubova.alina124@gmail.com>
2022-05-11 19:35:59 +0300

    First commit

---
+++ Two_sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    res = [i, j]
+        return res
+
+test = Solution()
+print(test.twoSum([2, 7, 11, 15], 9))
+print(test.twoSum([3, 2, 4], 6))
+print(test.twoSum([3, 3], 6))
